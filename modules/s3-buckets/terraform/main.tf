470fff623ac486bea473d2c6ebe52f26a7d82fda
Matthew Falcone <matthew.falcone@arcteryx.com>
2026-06-08 15:36:17 -0700

    Test atlantis

--- modules/s3-buckets/terraform/main.tf
+++ modules/s3-buckets/terraform/main.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "test_atlantis" {
   bucket = "test-atlantis-${var.environment}"
 }
+
+resource "aws_s3_bucket" "test_atlantis_2" {
+  bucket = "test-atlantis_2-${var.environment}"
+}
