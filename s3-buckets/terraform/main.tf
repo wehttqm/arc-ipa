21a849b97f30710a4c89dca56ccbb5c6ddc786b5
Matthew Falcone <matthew.falcone@arcteryx.com>
2026-06-08 15:48:04 -0700

    remove duplicate

--- s3-buckets/terraform/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "magento_order_history" {
-  bucket = "magento-order-history-${var.environment}"
-}
-
-resource "aws_s3_bucket" "switch_adapter" {
-  bucket = "switch-adapter-${var.environment}"
-}
