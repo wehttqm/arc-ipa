75a069d292389183742b319b4f273801db720f59
Matthew Falcone <matthew.falcone@arcteryx.com>
2026-06-16 18:25:45 -0400

    Remove unused atlassian secret

--- modules/secrets-manager/main.tf
+++ modules/secrets-manager/main.tf
@@ -8,16 +8,6 @@ resource "aws_secretsmanager_secret" "github_app" {
   }
 }
 
-resource "aws_secretsmanager_secret" "mcp" {
-  description = "secrets for infra agent mcp"
-  name        = "arc-ipa/mcp"
-  tags = {
-    env                        = var.env
-    team                       = var.team
-    owner                      = var.owner
-  }
-}
-
 # resource "aws_secretsmanager_secret_version" "github_app" {
 #   secret_id     = aws_secretsmanager_secret.github_app.id
 #   secret_string = jsonencode({
@@ -25,13 +15,5 @@ resource "aws_secretsmanager_secret" "mcp" {
 #     private_key     = ""
 #     installation_id = ""
 #     webhook_secret  = ""
-#   })
-# }
-
-# resource "aws_secretsmanager_secret_version" "mcp" {
-#   secret_id     = aws_secretsmanager_secret.mcp.id
-#   secret_string = jsonencode({
-#     atlassian_url   = ""
-#     atlassian_token = ""
 #   })
 # }
