056856412a7bf7a12c11faaa5dd14fd56507f238
Matthew Falcone <matthew.falcone@arcteryx.com>
2026-08-04 11:28:42 -0700

    Add atlassian app secrets

--- terraform/secrets-manager/terraform/main.tf
+++ terraform/secrets-manager/terraform/main.tf
@@ -8,6 +8,17 @@ resource "aws_secretsmanager_secret" "github_app" {
   }
 }
 
+resource "aws_secretsmanager_secret" "atlassian_app" {
+  description = "secrets for infra agent atlassian app"
+  name        = "arc-ipa/atlassian-app"
+  tags = {
+    env                        = var.env
+    team                       = var.team
+    owner                      = var.owner
+  }
+}
+
+
 
 resource "aws_secretsmanager_secret" "datadog_terraform_key" {
   description = "Datadog API key for OTEL trace export"
